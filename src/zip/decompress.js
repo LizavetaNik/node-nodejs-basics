b5500eb8d6e2b9fb4b39ce41b46e8e5720d22369
Lizaveta <lizabeth.nik@gmail.com>
2024-01-11 14:56:20 +0200

    feat: add decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,39 @@
+import {
+  createReadStream,
+  createWriteStream,
+  promises as fsPromises,
+} from "fs";
+import { join, dirname } from "path";
+import { pipeline } from "stream";
+import zlib from "zlib";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const decompress = async () => {
-    // Write your code here 
+  try {
+    const inputFilePath = join(__dirname, "files", "archive.gz");
+    const outputFilePath = join(__dirname, "files", "fileToCompress.txt");
+
+    const readStream = createReadStream(inputFilePath);
+
+    const writeStream = createWriteStream(outputFilePath);
+
+    const gunzipStream = zlib.createGunzip();
+
+    // Используем pipeline для удобного соединения потоков
+    await pipeline(readStream, gunzipStream, writeStream, (error) => {
+      if (error) {
+        console.error("Decompression failed:", error);
+      } else {
+        console.log("Decompression successful.");
+      }
+    });
+  } catch (error) {
+    console.error("Error during decompression:", error);
+    throw error;
+  }
 };
 
 await decompress();
