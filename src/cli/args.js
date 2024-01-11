5c88af4ba4ca2722280553a3f2a3800487ae3c61
Lizaveta <lizabeth.nik@gmail.com>
2024-01-11 10:57:11 +0200

    feat: add args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,27 @@
 const parseArgs = () => {
-    // Write your code here 
+  try {
+    const args = process.argv.slice(2);
+
+    if (args.length % 2 !== 0) {
+      throw new Error(
+        "Invalid number of arguments. Each property should have a corresponding value."
+      );
+    }
+
+    const parsedArgs = {};
+
+    for (let i = 0; i < args.length; i += 2) {
+      const propName = args[i].replace(/^--/, "");
+      const propValue = args[i + 1];
+      parsedArgs[propName] = propValue;
+    }
+
+    for (const [propName, propValue] of Object.entries(parsedArgs)) {
+      console.log(`${propName} is ${propValue}`);
+    }
+  } catch (err) {
+    console.error(err.message);
+  }
 };
 
 parseArgs();
