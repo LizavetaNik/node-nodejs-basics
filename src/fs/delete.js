2453366320e069115709f17429766528e4244bd4
Lizaveta <lizabeth.nik@gmail.com>
2024-01-11 10:29:09 +0200

    feat: add delete file

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,29 @@
+import { promises as fsPromises, constants } from "fs";
+import { fileURLToPath } from "url";
+import { join, dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const remove = async () => {
-    // Write your code here 
+  try {
+    const filePath = join(__dirname, "files", "fileToRemove.txt");
+
+    try {
+      await fsPromises.access(filePath, constants.F_OK);
+    } catch (error) {
+      if (error.code === "ENOENT") {
+        throw new Error("FS operation failed: File does not exist");
+      } else {
+        throw error;
+      }
+    }
+
+    await fsPromises.unlink(filePath);
+    console.log("File removed successfully.");
+  } catch (err) {
+    console.error(err.message);
+  }
 };
 
 await remove();
