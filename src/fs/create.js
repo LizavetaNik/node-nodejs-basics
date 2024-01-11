d940892aff625678b46695e85e7f74ee1d68c083
Lizaveta <lizabeth.nik@gmail.com>
2024-01-11 10:00:02 +0200

    feat: add create file

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,29 @@
+import { promises as fsPromises } from "fs";
+import { fileURLToPath } from "url";
+import { join, dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const create = async () => {
-    // Write your code here 
+  try {
+    const filePath = join(__dirname, "files", "fresh.txt");
+
+    try {
+      await fsPromises.readFile(filePath);
+
+      throw new Error("FS operation failed: File already exists");
+    } catch (error) {
+      if (error.code === "ENOENT") {
+        await fsPromises.writeFile(filePath, "I am fresh and young");
+        console.log("File created successfully: fresh.txt");
+      } else {
+        throw error;
+      }
+    }
+  } catch (err) {
+    console.error(err.message);
+  }
 };
 
 await create();
