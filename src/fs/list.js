cf67961cf33ee2bf3f49d26c1c89a461b59541e5
Lizaveta <lizabeth.nik@gmail.com>
2024-01-11 10:32:17 +0200

    feat: add list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,28 @@
+import { promises as fsPromises, constants } from "fs";
+import { fileURLToPath } from "url";
+import { join, dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const list = async () => {
-    // Write your code here 
+  try {
+    const folderPath = join(__dirname, "files");
+
+    await fsPromises.access(folderPath, constants.R_OK);
+
+    const files = await fsPromises.readdir(folderPath);
+    console.log('Files in the "files" folder:');
+    files.forEach((file) => {
+      console.log(file);
+    });
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      console.error("FS operation failed: Folder does not exist");
+    } else {
+      console.error(err.message);
+    }
+  }
 };
 
 await list();
