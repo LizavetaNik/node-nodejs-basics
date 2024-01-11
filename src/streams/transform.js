acd08d84e05d29eb62667f59dcf869ea57b54334
Lizaveta <lizabeth.nik@gmail.com>
2024-01-11 14:25:21 +0200

    feat: add streams-transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,21 @@
+import { Transform } from "stream";
+
 const transform = async () => {
-    // Write your code here 
+  try {
+    const reverseTransform = new Transform({
+      transform(chunk, encoding, callback) {
+        this.push(chunk.toString().split("").reverse().join(""));
+        callback();
+      },
+    });
+
+    process.stdin.pipe(reverseTransform).pipe(process.stdout);
+
+    console.log("Enter text (Enter for give ansve, Ctrl + C for end):");
+  } catch (error) {
+    console.error("Error during transformation:", error);
+    throw error;
+  }
 };
 
 await transform();
